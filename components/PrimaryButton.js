fe35d43ba05d395eee866a4943eabc1abc49d385
Aki-Coder <anchicabeybe@gmail.com>
2023-01-22 21:49:03 +0100

    handle user input (state & props)

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,16 +1,11 @@
 import { View, Text, Pressable, StyleSheet } from "react-native";
                     //umesto props.children moze i ovako destruktuiranje objekta
-function PrimaryButton({children}){
-
-    function pressHandler(){
-        console.log('hi');
-    }
-
+function PrimaryButton({children, onPress}){
 
     return (
         <View style={styles.buttonOuterContainer}>
             <Pressable 
-                onPress={pressHandler}
+                onPress={onPress}
                 android_ripple={{color:'#640233'}}
                 style={({pressed}) => 
                                 pressed ? [styles.buttonInnerContainer, styles.pressed] 
